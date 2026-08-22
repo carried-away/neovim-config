50b9a4644a37d32b6207baf5bb5a823bf54f447e
carried-away <119022007+carried-away@users.noreply.github.com>
2026-08-22 17:33:46 -0400

    feat(config): add custom git keymaps, navigation improvements, and editor options
    - Keymaps:
      - Add Gitsigns mappings for blame, diff, and hunks
      - Center cursor (`zz`) during `<C-u>` and `<C-d>` half-page scrolling
      - Map `<leader>gg` to launch CodeCompanion commit prompt
    - Options:
      - Enable spellchecking dynamically for HTML and Markdown
      - Disable spellchecking in terminal windows
      - Disable automatic comment continuation (`formatoptions-=cro`)
      - Force line wrapping layout via `linebreak`

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("n", "<leader>gb", "<cmd> Gitsigns blame_line <cr>", { desc = "Git Blame Line" })
+map("n", "<leader>gB", "<cmd> Gitsigns toggle_current_line_blame <cr>", { desc = "Git Toggle Blame Line" })
+map("n", "<leader>gf", "<cmd> Gitsigns blame <cr>", { desc = "Git Blame File" })
+map("n", "<leader>gd", "<cmd> Gitsigns diffthis <cr>", { desc = "Git Diff This" })
+map("n", "<leader>gh", "<cmd> Gitsigns preview_hunk <cr>", { desc = "Git Preview Hunk" })
+
+-- Navigation
+map("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true, desc = "Move Up half a page" })
+map("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true, desc = "Move Down half a page" })
+
+map("n", "<leader>gg", function()
+  require("codecompanion").prompt("commit")
+end, { noremap = true, silent = true })
+
+
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,2 +1,40 @@
 -- Options are automatically loaded before lazy.nvim startup.
 require("config.remote_clipboard").setup()
+
+local cmd = vim.cmd
+local g = vim.g
+local o = vim.o
+local opt = vim.opt
+local autocmd = vim.api.nvim_create_autocmd
+
+opt.relativenumber = false
+vim.g.autoformat = false
+
+-- spellchecker
+autocmd('FileType', {
+  pattern = { 'html', 'markdown' },
+  callback = function()
+    vim.opt.spell = true
+    vim.opt.spelllang = { 'en_us' }
+  end,
+})
+
+-- turn off spellchecker in terminal
+autocmd('TermOpen', {
+  pattern = '*',
+  command = 'setlocal nospell',
+})
+
+-- When in a comment and enter, o or O are pressed, do not auto-comment
+autocmd('FileType', {
+  command = 'set formatoptions-=cro',
+})
+
+opt.incsearch = true
+opt.hlsearch = true
+opt.relativenumber = true
+opt.linebreak = true
+
+
+
+
